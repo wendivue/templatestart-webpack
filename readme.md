4590d1f4e49a4f8f228e3edbac6709bc9809f256
wendivue <vovas2345@gmail.com>
2020-10-07 23:44:18 +1000

    add: eslint & prettier & babel and update dependencies

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Template-Webpack
+
+## Installing
+Install Node Modules: <code>npm i</code>
+
+Run dev-server: <code>npm run start</code>
+
+Development: <code>npm run dev</code>
+
+Development-watch: <code>npm run watch</code>
+
+Production: <code>npm run build</code>
--- webpack.config.js
+++ webpack.config.js
@@ -1,116 +1,92 @@
-const path = require("path");
-const fs = require("fs");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-
-function generateHtmlPlugins(templateDir) {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
-    const parts = item.split(".");
-    const name = parts[0];
-    const extension = parts[1];
-    return new HtmlWebpackPlugin({
-      filename: `${name}.html`,
-      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-      inject: false
-    });
-  });
-}
-
-const htmlPlugins = generateHtmlPlugins("./src/html/views");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 const config = {
-  entry: ["./src/js/index.js", "./src/scss/main.scss"],
+  entry: ['./src/js/index.js', './src/scss/style.scss'],
   output: {
-    filename: "./js/bundle.js"
+    filename: './js/bundle.js',
   },
-  devtool: "source-map",
-  mode: "production",
+  devtool: 'source-map',
+  mode: 'production',
   optimization: {
     minimizer: [
       new TerserPlugin({
         sourceMap: true,
-        extractComments: true
-      })
-    ]
+        extractComments: true,
+      }),
+    ],
   },
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, "src/scss"),
+        include: path.resolve(__dirname, 'src/scss'),
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {}
+            options: {},
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               sourceMap: true,
-              url: false
-            }
+              url: false,
+            },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
-              ident: "postcss",
+              postcssOptions: {
+                plugins: [
+                  ['postcss-preset-env', { browsers: 'last 2 versions' }],
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
               sourceMap: true,
-              plugins: () => [
-                require("cssnano")({
-                  preset: [
-                    "default",
-                    {
-                      discardComments: {
-                        removeAll: true
-                      }
-                    }
-                  ]
-                })
-              ]
-            }
+            },
           },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
+        ],
       },
-      {
-        test: /\.html$/,
-        include: path.resolve(__dirname, "src/html/includes"),
-        use: ["raw-loader"]
-      }
-    ]
+    ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "./css/main.bundle.css"
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery',
     }),
-    new CopyWebpackPlugin([
-      {
-        from: "./src/fonts",
-        to: "./fonts"
-      },
-      {
-        from: "./src/favicon",
-        to: "./favicon"
-      },
-      {
-        from: "./src/img",
-        to: "./img"
-      }
-    ])
-  ].concat(htmlPlugins)
+    new MiniCssExtractPlugin({
+      filename: './css/style.bundle.css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/template/index.html',
+    }),
+  ],
 };
 
 module.exports = (env, argv) => {
-  if (argv.mode === "production") {
+  if (argv.mode === 'production') {
     config.plugins.push(new CleanWebpackPlugin());
   }
   return config;
